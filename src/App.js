4c2b065ec5d4db824fa9f30cb125df3c7c3b35dd
clz03 <cleber.znch@gmail.com>
2023-11-29 21:09:23 -0300

    add changes to netlify

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React, { Component } from 'react'
 import Rotas from '../src/routes';
-import './App.css';
 
-function App() {
-  return (
-    <Rotas/>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Rotas/>
+      </div>
+    )
+  }
 }
-
-export default App;
