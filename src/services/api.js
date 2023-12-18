b5cbc38a81d9b53d81cfb2c3aa61712d821e9e8b
clz03 <cleber.znch@gmail.com>
2023-12-18 16:11:58 -0300

    add requests

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    //baseURL: "https://wcqiplataforma.herokuapp.com/"
-    baseURL: "http://localhost:8080/"
+    baseURL: "https://wcqiplataforma.herokuapp.com/"
+    // baseURL: "http://localhost:8080/"
 })
 
 export default api;
